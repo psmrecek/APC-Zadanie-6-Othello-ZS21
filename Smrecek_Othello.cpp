b09c25cf56e76504806667d4055b4b64aa88641e
Peter Smrecek <petersmrecek@gmail.com>
2021-12-28 16:09:16 +0100

    Added assignment text

---
+++ Smrecek_Othello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <regex>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
